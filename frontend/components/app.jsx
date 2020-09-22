2c5403ae654e68fb257e0b10be0788f8b06551aa
travis-holter <travisjh@gmail.com>
2020-09-22 14:24:15 -0700

    Sign up and logout work

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,15 +5,16 @@ import SignupContainer from './session/signup_container';
 import Home from './home/home';
 import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from "../utils/route_utils"
+import GreetingContainer from './greeting/greeting_container';
 
 export default () => {
     return(
     <div>
         <Route path="/" component={MainNavContainer} />
         <Route exact path="/" component={Home} />
-        <Route path="/signup" component={SignupContainer} />
+        {/* <Route path="/signup" component={SignupContainer} /> */}
         {/* <Route path="/login" component={SignupContainer} /> */}
-        {/* <AuthRoute path="/signup" component={SignupContainer} /> */}
+        <AuthRoute path="/signup" component={SignupContainer} />
     </div>
     )
 };
--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -19,8 +19,7 @@ class Signup extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        this.props.createNewUser(this.state)
-            .then(() => this.props.history.push('/chirps'));
+        this.props.createNewUser(this.state);
     }
 
     render() {
