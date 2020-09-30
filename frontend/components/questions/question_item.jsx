649bd743d1e3440253bab1b040c4def4fe9eae82
travis-holter <travisjh@gmail.com>
2020-09-30 13:36:04 -0700

    made date/time stuff work

--- frontend/components/questions/question_item.jsx
+++ frontend/components/questions/question_item.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { formatDate, timeAgo } from '../../utils/date_util';
 
-const QuestionItem = ({question}) => {
+const QuestionItem = ({ question}) => {
     return (
         <li className="question-item">
-            <div className='question-item-div'>
-                <p className='number-gray-txt'>0</p>
-                <p className='little-gray-txt'>votes</p>
-            </div>
-            <div className='question-item-div'>
-                <p className='number-gray-txt'>0</p>
-                <p className='little-gray-txt'>answers</p>
+            <div className='stats-div'>
+                <div className='question-item-div-numbers'>
+                    <p className='number-gray-txt'>0</p>
+                    <p className='little-gray-txt'>votes</p>
+                </div>
+                <div className='question-item-div-numbers'>
+                    <p className='number-gray-txt'>0</p>
+                    <p className='little-gray-txt'>answers</p>
+                </div>
             </div>
             <div className='question-item-div'>
                 <p>{question.title}</p>
-                <p className='gray-txt'>created a bit ago by some guy</p>
+                <p className='gray-txt'>Created {timeAgo(question.created_at)} ago</p>
             </div>
         </li>
     )
---
+++ frontend/utils/date_util.js
@@ -0,0 +1,71 @@
+export const formatDate = date => {
+
+    const months = {
+        0: 'January',
+        1: 'February',
+        2: 'March',
+        3: 'April',
+        4: 'May',
+        5: 'June',
+        6: 'July',
+        7: 'August',
+        8: 'September',
+        9: 'October',
+        10: 'November',
+        11: 'December',
+    };
+
+    const daysOfWeek = {
+        0: 'Sunday',
+        1: 'Monday',
+        2: 'Tuesday',
+        3: 'Wednesday',
+        4: 'Thursday',
+        5: 'Friday',
+        6: 'Saturday',
+    };
+
+    const obj = new Date(date);
+    const month = months[obj.getMonth()];
+    const day = obj.getDate();
+    const year = obj.getFullYear();
+    const dayOfWeek = daysOfWeek[obj.getDay()];
+    return `${month} ${day}, ${year} (${dayOfWeek})`;
+
+};
+
+export const timeAgo = date => {
+    const current = new Date();
+    const old = new Date(date);
+    if(old.getFullYear() < current.getFullYear){
+        return(
+            `${current.getFullYear() - old.getFullYear()} years`
+        )
+    }
+    if(old.getMonth() < current.getMonth()){
+        return(
+            `${current.getMonth() - old.getMonth()} months`
+        )
+    }
+    if (old.getDay() < current.getDay()) {
+        return (
+            `${current.getDay() - old.getDay()} days`
+        )
+    }
+    if (old.getHours() < current.getHours()) {
+        return (
+            `${current.getHours() - old.getHours()} hours`
+        )
+    }
+    if (old.getMinutes() < current.getMinutes()) {
+        return (
+            `${current.getMinutes() - old.getMinutes()} minutes`
+        )
+    }
+    if (old.getSeconds() < current.getSeconds()) {
+        return (
+            `${current.getSeconds() - old.getSeconds()} seconds`
+        )
+    }
+};
+
